9db37823a96b009ea81265b9f07917ebd827c6e4
sheferr <roman.shefer@iu-study.org>
2023-08-19 16:53:51 +0200

    Mobile navigation implementiert

--- src/Header.js
+++ src/Header.js
@@ -9,6 +9,9 @@ import Container from '@mui/material/Container';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
 
 // Logo importieren
 import Image from './img/GC_Logo.png';
@@ -24,15 +27,30 @@ function Logo(props) {
   )
 }
 
-function handleClickScroll(id) {
-  const element = document.getElementById(id);
-  if (element) {
-    element.scrollIntoView({ behavior: 'smooth' });
-  }
-}
-
 
 export default function Header() {
+
+  const pages = ['Über uns', 'CO²-Emissionen', 'Kontakt'];
+  const page_id = ['id-about', 'id-emissions', 'id-contact'];
+
+  const [anchorNav, setAnchorNav] = React.useState(null);
+
+  function handleClickScroll(id) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+    setAnchorNav(null);
+  }
+
+  const handleOpenNavBar = (event) => {
+    setAnchorNav(event.currentTarget);
+  }
+
+  const handleCloseNavBar = () => {
+    setAnchorNav(null);
+  }
+
   return (
     <AppBar position='static' sx={{ py: 1 }}>
       <Container maxWidth="lg" >
@@ -43,18 +61,24 @@ export default function Header() {
 
           {/* Desktop-Ansicht */}
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Button size="large" onClick={() => handleClickScroll("id-about")}> Über uns </Button>
-            <Button size="large" onClick={() => handleClickScroll("id-emissions")}> CO²-Emissionen </Button>
-            <Button size="large" onClick={() => handleClickScroll("id-contact")}> Kontakt </Button>
+            {pages.map((page, index) => (
+              <Button key={page} size="large" onClick={() => handleClickScroll(page_id[index])}>{page}</Button>
+            ))}
           </Box>
 
           {/* Mobile-Ansicht */}
           <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton color='primary'>
+            <IconButton color='primary' onClick={handleOpenNavBar}>
               <MenuIcon />
             </IconButton>
+            <Menu anchorEl={anchorNav} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left', }} open={Boolean(anchorNav)} onClose={handleCloseNavBar} sx={{ display: { xs: 'block', md: 'none' } }}>
+              {pages.map((page, index) => (
+                <MenuItem key={page} onClick={() => handleClickScroll(page_id[index])}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Box>
-
         </Toolbar>
       </Container>
     </AppBar>
